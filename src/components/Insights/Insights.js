8135fdc642175546804a2b11b866075a13aaf1b1
Dane <dane.gled@gmail.com>
2022-08-21 02:55:17 -0400

    Add image references

--- src/components/Insights/Insights.js
+++ src/components/Insights/Insights.js
@@ -7,17 +7,21 @@ const Insights = () => (
     <br/>
     <h4 className='pageSubTitle'>Mood Art</h4>
     <div className='moodArtContainer'>
-
+      <img src='./../../../public/moodImage.jpeg' alt='Mood art'/>
     </div>
     <h4 className='pageSubTitle'>Predictions</h4>
     <div className='predictionGraphContainer'>
-      <div className='graphContainer'></div>
-      <div className='graphContainer'></div>
+      <div className='graphContainer'>
+        <img src='./../../../public/graph1.png' alt='Bar graph of most common words used'/>
+      </div>
+      <div className='graphContainer'>
+        <img src='./../../../public/graph2.svg' alt='Pie chart of predicted emotions'/>
+      </div>
     </div>
     <h4 className='pageSubTitle'>Articles</h4>
     <div className='articleContainer'>
       <h3>Tips for Anxiety</h3>
-      <img src='./../../../public/logo192.png' alt='anxiety'/>
+      <img src='./../../../public/anxiety.jpeg' alt='Anxiety article'/>
       <p>Anxiety can come at any time, it is important to always be prepared with coping strategies. Click to learn more!</p>
     </div>
   </div>
