8736b58a5703a7526513274d8e1e03186e0f2830
Dane <dane.gled@gmail.com>
2022-08-21 02:25:05 -0400

    Add insights content shapes

--- src/components/Insights/Insights.js
+++ src/components/Insights/Insights.js
@@ -6,6 +6,17 @@ const Insights = () => (
   <div className={styles.Insights}>
     <h1 className='pageTitle'>Insights</h1>
     <br/>
+    <h4 className='pageSubTitle'>Mood Art</h4>
+    <div className='moodArtContainer'>
+
+    </div>
+    <h4 className='pageSubTitle'>Predictions</h4>
+    <div className='predictionGraphContainer'>
+      <div className='graphContainer'></div>
+      <div className='graphContainer'></div>
+    </div>
+    <h4 className='pageSubTitle'>Articles</h4>
+    <div className='articleContainer'></div>
   </div>
 );
 
